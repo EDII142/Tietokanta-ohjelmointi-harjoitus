23495317c4a4f9803c60b975c37228bee1b20788
Eetu Huotari <eetu142@gmail.com>
2023-04-08 15:17:28 +0300

    Added remove_artist.php

--- remove_artist.php
+++ remove_artist.php
@@ -3,3 +3,16 @@
 require "./Tietokanta-ohjelmointi-harjoitus/dbconnection.php";
 $dbcon = createDbConnection();
 
+$aristId = "artist_id";
+
+try {
+    $dbcon->beginTransaction();
+
+    $statement = $dbcon->prepare("DELETE FROM artists WHERE artistId = $aristId");
+    $statement->execute(array($artistId));
+
+    $dbcon->commit();
+} catch(Exception $e) {
+    $dbcon->rollBack();
+    echo $e->getMessage();
+}
